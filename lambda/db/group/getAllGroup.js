c6326dd43134b81af49ceb102c8f133bc9426d05
jane-k <64597426+jane-k@users.noreply.github.com>
2021-04-14 19:22:18 +0900

    Create getAllGroup.js

---
+++ lambda/db/group/getAllGroup.js
@@ -0,0 +1,45 @@
+var AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: 'ap-northeast-2',
+    endpoint: "http://dynamodb.ap-northeast-2.amazonaws.com"
+})
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+var response = {
+    "statusCode": 200,
+    "headers": {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        "GET": "success",
+    },
+    "isBase64Encoded": false
+};
+
+var failResponse = {
+    "statusCode": 400,
+    "headers": {
+        "GET": "fail"
+    },
+    "isBase64Encoded": false
+};
+
+exports.handler = function(event, context, callback) {
+    var params = {
+        TableName: 'groupDataBase',
+        Select: 'ALL_ATTRIBUTES'
+    };
+    dynamo.scan(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+            failResponse.body = JSON.stringify({"message": `has error: ${err}`});
+            callback(null, failResponse);
+        } else {
+            console.log("Success", data);
+            response.body = JSON.stringify(data);
+            callback(null, response);
+        }
+    });
+}
