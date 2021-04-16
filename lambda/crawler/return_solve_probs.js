77fc14a0f439d553fa21f0873d8e7fac284f280b
kimtaehyun98 <65909160+kimtaehyun98@users.noreply.github.com>
2021-04-16 14:25:37 +0900

    Update return_solve_probs.js

--- lambda/crawler/return_solve_probs.js
+++ lambda/crawler/return_solve_probs.js
@@ -14,7 +14,7 @@ exports.handler = async (event, context, callback) => {
         console.log("Success!!");
 
          // cheerio 모듈을 사용하여 data를 저장하고
-        var $ = cheerio.load(all_data.data);
+        let $ = cheerio.load(all_data.data);
         // 해당 string은 내가 원하는 정보인 "지금까지 solve한 문제들"이 저장되어 있는 경로임!
         $('body>div.wrapper>div.container>div.row>div.col-md-12>div.row>div.col-md-9>div.panel:nth-child(1)>div.panel-body>a').each((index,item) => {
             solve_num.push(item.attribs.href.substr(9)); // 지금까지 푼 문제 번호들 저장
