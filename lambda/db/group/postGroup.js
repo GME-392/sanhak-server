8b12cf8d911b581e8fb27c40d95f166dd980bc27
jane-k <64597426+jane-k@users.noreply.github.com>
2021-04-24 21:31:25 +0900

    Update postGroup.js

--- lambda/db/group/postGroup.js
+++ lambda/db/group/postGroup.js
@@ -33,63 +33,16 @@ exports.handler = function(event, context, callback) {
             "id": event.id,
             "leader": event.leader,
             "name": event.name,
-            "numberOfMember": event.numberOfMember,
             "member": event.member,
-            "rank_member": event.rank_member,
-            "rank": event.rank,
-            "group_goal": event.group_goal,
-            "group_attendance": event.group_attendance,
+            "rank_member": null,
+            "rank_group": null,
+            "group_goal": null,
+            "group_attendance": null,
             "group_info": event.group_info,
             "max_member": event.max_member,
             "tag": event.tag,
-            "group_status": event.group_status
-        }
-    };
-    var AWS = require('aws-sdk')
-
-AWS.config.update({
-    region: 'ap-northeast-2',
-    endpoint: "http://dynamodb.ap-northeast-2.amazonaws.com"
-})
-
-let response = {
-    "statusCode": 200,
-    "headers": {
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        "GET": "success",
-    },
-    "isBase64Encoded": false
-};
-
-let failResponse = {
-    "statusCode": 400,
-    "headers": {
-        "GET": "fail"
-    },
-    "isBase64Encoded": false
-};
-
-const dynamo = new AWS.DynamoDB.DocumentClient();
-exports.handler = function(event, context, callback) {
-    console.log(event);
-    var params = {
-        TableName: "groupDataBase",
-        Item: {
-            "id": event.id,
-            "leader": event.leader,
-            "name": event.name,
-            "numberOfMember": event.numberOfMember,
-            "member": [],
-            "rank_member": event.rank_member,
-            "rank": event.rank,
-            "group_goal": event.group_goal,
-            "group_attendance": event.group_attendance,
-            "group_info": event.group_info,
-            "max_member": event.max_member,
-            "tag": event.tag,
-            "group_status": event.group_status
+            "group_status": null,
+            "group_noti": null
         }
     };
     
@@ -108,18 +61,4 @@ exports.handler = function(event, context, callback) {
         }
     });
 }
-    
 
-    dynamo.put(params, function(err, data) {
-        if (err) {
-            console.log("Error", err);
-            failResponse.body = JSON.stringify({"message": `has error: ${err}`});
-            callback(null, failResponse);
-            
-        } else {
-            console.log("Success", data);
-            response.body = JSON.stringify(data);
-            callback(null, response);
-        }
-    });
-}
