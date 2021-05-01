86a6758eb4cf1ef7c0e23d7bac5b935e108804d9
jane-k <64597426+jane-k@users.noreply.github.com>
2021-05-01 23:50:52 +0900

    Update getGroup.js

--- lambda/db/group/getGroup.js
+++ lambda/db/group/getGroup.js
@@ -27,50 +27,6 @@ var failResponse = {
 };
 
 
-
-exports.handler = function(event, context, callback) {
-    console.log(event);
-    var params = {
-        TableName: "groupDataBase",
-        Key: {
-            "id": event.id
-            
-        }
-        
-    };
-    dynamo.get(params, function(err, data) {
-        if (err) {
-            console.log("Error", err);
-            failResponse.body = JSON.stringify({"message": `has error: ${err}`});
-            callback(null, failResponse);var AWS = require('aws-sdk');
-// Set the region 
-AWS.config.update({
-    region: 'ap-northeast-2',
-    endpoint: "http://dynamodb.ap-northeast-2.amazonaws.com"
-})
-// Create the DynamoDB service object
-const dynamo = new AWS.DynamoDB.DocumentClient();
-
-var response = {
-    "statusCode": 200,
-    "headers": {
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        "GET": "success",
-    },
-    "isBase64Encoded": false
-};
-
-var failResponse = {
-    "statusCode": 400,
-    "headers": {
-        "GET": "fail"
-    },
-    "isBase64Encoded": false
-};
-
-
 exports.handler = function(event, context, callback) {
     var id = event.queryStringParameters.id;
     var func = event.queryStringParameters.func;
