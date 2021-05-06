b9fad6370ea7e658db5a4c5e4d3d4201d28ffb87
jane-k <64597426+jane-k@users.noreply.github.com>
2021-05-06 23:58:06 +0900

    Update postGroup.js

--- lambda/db/group/postGroup.js
+++ lambda/db/group/postGroup.js
@@ -27,14 +27,22 @@ let failResponse = {
 const dynamo = new AWS.DynamoDB.DocumentClient();
 exports.handler = function(event, context, callback) {
     console.log(event);
+    var member_name = event.member;
+    var boj_id = event.boj_id;
+    var rank_member = {};
+    var member_info = {};
+    for(var i=0; i<member_name.length; i++){
+        member_info[member_name[i]] = {"boj_id": boj_id[i], "score":0, "attendance":"absent"};
+    }
+    Object.assign(rank_member, member_info);
     var params = {
         TableName: "groupDataBase",
         Item: {
             "id": event.id,
             "leader": event.leader,
             "name": event.name,
-            "member": event.member,
-            "rank_member": {},
+            "member": member_name,
+            "rank_member": rank_member,
             "rank_group": null,
             "group_goal": {},
             "group_attendance": {"attendance_cycle": 7,"probs":[]},
@@ -44,12 +52,10 @@ exports.handler = function(event, context, callback) {
             "group_status": null,
             "group_noti": null,
             "group_type": event.group_type,
-            "problems": []
+            "problems": [],
+            "group_point": null
         }
     };
-    
-    
-
     dynamo.put(params, function(err, data) {
         if (err) {
             console.log("Error", err);
