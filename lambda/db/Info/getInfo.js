0707d57b3eddebb9129b8333fc2ba9f3a97c0fa8
jane-k <64597426+jane-k@users.noreply.github.com>
2021-05-08 13:59:33 +0900

    Create getInfo.js

---
+++ lambda/db/Info/getInfo.js
@@ -0,0 +1,83 @@
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({
+    region: 'ap-northeast-2',
+    endpoint: "http://dynamodb.ap-northeast-2.amazonaws.com"
+})
+// Create the DynamoDB service object
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+var response = {
+    "statusCode": 200,
+    "headers": {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        "GET": "success",
+    },
+    "isBase64Encoded": false
+};
+
+var failResponse = {
+    "statusCode": 400,
+    "headers": {
+        "GET": "fail"
+    },
+    "isBase64Encoded": false
+};
+
+
+exports.handler = function(event, context, callback) {
+    var id = event.queryStringParameters.id;
+    var func = event.queryStringParameters.func;
+
+    switch (func) {
+        case 'getInfo':
+            getNotice(id, callback);
+            break;
+        
+        case 'getAllInfo':
+            getAllNotice(callback);
+            break;
+        
+    }
+};
+
+function getNotice(id, callback){
+    var params = {
+        TableName: 'infoDB',
+        Key: {
+            "id": id
+            
+        }
+        
+    };
+    dynamo.get(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+            failResponse.body = JSON.stringify({"message": `has error: ${err}`});
+            callback(null, failResponse);
+        } else {
+            console.log("Success", data);
+            response.body = JSON.stringify(data);
+            callback(null, response);
+        }
+    });
+}
+function getAllNotice(callback){
+    var params = {
+        TableName: 'infoDB',
+        Select: 'ALL_ATTRIBUTES'
+    };
+    dynamo.scan(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+            failResponse.body = JSON.stringify({"message": `has error: ${err}`});
+            callback(null, failResponse);
+        } else {
+            console.log("Success", data);
+            response.body = JSON.stringify(data.Items);
+            callback(null, response);
+        }
+    });
+}
